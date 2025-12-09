409b5f38eac039ecb455c93a0815db6d28842908
Suganth V <suganth.v2024cse@sece.ac.in>
2025-12-09 15:31:12 +0530

    DAY 7 - React Second app

--- DAY-7/second_app/src/App.jsx
+++ DAY-7/second_app/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 		{ id: 3, name: "Surya", fee: false }
 	]);
 
+	const [searchQuery, setSearchQuery] = useState("");
+
 	const handleDelete = (id) => {
 		setCount(count.filter((ls) => ls.id !== id))
 	}
@@ -16,19 +18,37 @@ function App() {
 		setCount(count.map((ls) => ls.id === id ? { ...ls, fee: !ls.fee } : ls))
 	}
 
+	const handleAdd = (name) => {
+		setCount([...count, { id: count.length + 1, name: name, fee: false }])
+		val.value = ""
+	}
+
+	const filteredStudents = count.filter((ls) =>
+		ls.name.toLowerCase().includes(searchQuery.toLowerCase())
+	);
+
 	return (
 		<div className="app-container">
 			<div className="card">
 				<h1 className="title">Student List</h1>
-				<ul className="student-list"> {count.map((ls) => (
-						<li key={ls.id} className="student-item">
-							<div className="student-info">
-								<input type="checkbox" checked={ls.fee} onChange={() => handleCheck(ls.id)} />
-								<label>{ls.name}</label>
-							</div>
-							<button className="delete-btn" onClick={() => handleDelete(ls.id)}>Delete</button>
-						</li>
-					))}
+				<input id='val' type="text" onChange={(e) => setName(e.target.value)} placeholder="Enter student name" />
+				<button className="add-btn" onClick={() => handleAdd(val.value)}>Add Student</button>
+				<input
+					type="text"
+					className="search-input"
+					onChange={(e) => setSearchQuery(e.target.value)}
+					placeholder="🔍 Search"
+					value={searchQuery}
+				/>
+				<ul className="student-list"> {filteredStudents.map((ls) => (
+					<li key={ls.id} className="student-item">
+						<div className="student-info">
+							<input type="checkbox" checked={ls.fee} onChange={() => handleCheck(ls.id)} />
+							<label>{ls.name}</label>
+						</div>
+						<button className="delete-btn" onClick={() => handleDelete(ls.id)}>Delete</button>
+					</li>
+				))}
 				</ul>
 				<p className='fee-paid'>Fee Paid: {count.filter((ls) => ls.fee).length}</p>
 				<p className='fee-not-paid'>Fee Not Paid: {count.filter((ls) => !ls.fee).length}</p>

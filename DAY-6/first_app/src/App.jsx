2989a5b8356e652ad5d91bf454cc81c9c0d2368a
Suganth V <suganth.v2024cse@sece.ac.in>
2025-12-10 14:09:42 +0530

    DAY 8 - Progress

--- DAY-6/first_app/src/App.jsx
+++ DAY-6/first_app/src/App.jsx
@@ -7,9 +7,8 @@ import Body from "./Body";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<h2><center>Welcome Home 👋</center></h2>} />
+        <Route path="/" element={<Counter />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/form" element={<Body />} />
       </Routes>
